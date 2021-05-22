817e0731294d2f76c1fd867e877491de192f778c
happy663 <53854298+happy663@users.noreply.github.com>
2021-05-23 06:11:52 +0900

    v-appの理解

---
+++ front/layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app :style="{ background: $vuetify.theme.themes.light.background }">
+    <v-app-bar color="primary">
+      <v-toolbar-title>Vuetify</v-toolbar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt :style="{ background: $vuetify.theme.themes.light.background }" />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Welcome",
+          to: "/"
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Inspire",
+          to: "/inspire"
+        }
+      ],
+      miniVariant: false,
+
+      right: true,
+      rightDrawer: false,
+      title: "Vuetify.js"
+    };
+  }
+};
+</script>
---
+++ front/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <h1>{{ message }}</h1>
+    <h1>saofuasufasoua</h1>
+    <input type="text" v-model="message" />
+  </v-app>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+
+export default {
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  data() {
+    return {
+      name: "",
+      size: "",
+      message: "hhhhhh"
+    };
+  },
+  methods: {
+    chnageName: function() {
+      this.message = "おはようございます";
+    }
+  },
+  created() {}
+};
+</script>
